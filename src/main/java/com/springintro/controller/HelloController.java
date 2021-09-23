d2ea6ae0f17f93468b1027989070419433a347cc
Priteshraj Bhangale <priteshraj.bhangale@morningstar.com>
2021-09-23 16:55:36 +0530

    [Pritesh] Displayed Hello from Bridgelabs with help of model

--- src/main/java/com/springintro/controller/HelloController.java
+++ src/main/java/com/springintro/controller/HelloController.java
@@ -1,14 +1,15 @@
 package com.springintro.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/get")
 public class HelloController {
 
 	@RequestMapping("/")
-	public String Hello() {
-		System.out.println("1234............");
+	public String Hello(Model mo) {
+		mo.addAttribute("msg", "Hello from BridgeLabs");
 		return "welcome";
 	}
 }
